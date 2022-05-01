225d51fded17ec57bf90871dead2190af87e9634
Tyllis Xu <LivelyCarpet87@gmail.com>
2022-05-01 20:44:45 +0800

    Fixed Server to communicate with mindustry via local TCP IO port

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
-import pexpect
 import flask
 from flask import render_template, abort, redirect, request, make_response, send_file, g, session, flash
-import os
+import os, socket, subprocess, time, re
 
 # START CONFIG HERE
 
@@ -19,10 +18,23 @@ desc="A private server for friends."
 # Memory limit when running server. Useful for limited available memory
 memoryLimit="-Xmx200M"
 
+# Maximum amount of saved server interactions
+maxConvoLen=20
+
 # User accounts on the server
 accounts = {
-    "admin": "PASSWORD_CHANGE_ME_PLEASE",
-    "invitee": "CHANGE_THIS_TOO" 
+    "admin": {
+        "password": "PASSWORD_CHANGE_ME_PLEASE",
+        "allowedCommands": ["pause", "host", "stop", "maps", "status"],
+        "allowInput": True,
+        "seeFullHistory": True,
+        },
+    "invitee": {
+        "password": "PASSWORD_CHANGE_ME_PLEASE",
+        "allowedCommands": [],
+        "allowInput": False,
+        "seeFullHistory": False,
+        },
 }
 secret_key="CHANGEMETOO"
 # END CONFIG HERE
@@ -31,33 +43,82 @@ if jar_path == "/path/to/server-release.jar":
     print("Open the server.py file to edit the configuration.")
     exit()
 
-child = None
+mindustrySocket = None
 app = flask.Flask(__name__, static_folder='assets')
 app.secret_key = secret_key
 port = int(os.getenv('PORT', 8090))
 logHead=r"\[\d\d\-\d\d\-\d\d\d\d \d\d:\d\d:\d\d\] \[I\]\ "
-import time
+conversation=[]
+conversationPointer=-1
+maps=[]
+
+def inputCommand(command, byUser=True):
+    global conversationPointer
+    mindustrySocket.sendall(command.encode()+b'\n')
+    if byUser:
+        conversationPointer += 1
+    return getOutput()
+
+def getOutput():
+    global conversation, conversationPointer
+    newExchange = b""
+    while True:
+        try:
+            newExchange += mindustrySocket.recv(1)
+        except TimeoutError:
+            break
+    newExchange = re.sub(r'\x1b\[\d+m','',newExchange.decode())
+    newExchange = newExchange
+    if newExchange != "":
+        conversationPointer -= 1
+        if len(conversation) <= maxConvoLen:
+            conversation.append(newExchange)
+        else:
+            conversation.pop(0)
+            conversation.append(newExchange)
+    else:
+        conversationPointer -= 1
+        if len(conversation) <= maxConvoLen:
+            conversation.append("Server timed out")
+        else:
+            conversation.pop(0)
+            conversation.append("Server timed out")
+    return newExchange
+
+
+
 
 @app.before_first_request
 def init():
-    global child
-    child = pexpect.spawn(f'{java_path} {memoryLimit} -jar {jar_path}')
-    child.sendline(f'config name {serverName}')
-    child.sendline(f'config description {desc}')
-    child.sendline('config whitelist true')
+    global mindustrySocket, maps
+    child = subprocess.Popen([java_path, memoryLimit, "-jar", jar_path], stdin=subprocess.PIPE, 
+    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    child.stdin.write(f'config name {serverName}\n'.encode())
+    child.stdin.write(f'config description {desc}\n'.encode())
+    child.stdin.write(b'config whitelist true\n')
+    child.stdin.write(b'config socketInput true\n')
+    time.sleep(1)
+    mindustrySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mindustrySocket.connect( ("localhost", 6859) )
+    mindustrySocket.settimeout(5.0)
+
+    mapsCommandOutput = inputCommand("maps")
+    matches = re.findall(r"([A-Za-z_]*):\ [DefaultCustom]* \/ \d+x\d+", mapsCommandOutput)
+    for match in matches:
+        maps.append(match)
 
 @app.route('/', methods=['GET'])
 def home():
     username = session.get("username")
     if username is None:
         return render_template("login.html")
-    return render_template("home.html")
+    return render_template("home.html", maps=maps, conversation = conversation, conversationPointer=conversationPointer)
 
 @app.route('/', methods=['POST'])
 def login():
     usernameIn = request.form.get('username')
     passwordIn = request.form.get('password')
-    if usernameIn in accounts.keys() and accounts[usernameIn] == passwordIn:
+    if usernameIn in accounts.keys() and accounts[usernameIn]["password"] == passwordIn:
         session["username"]=usernameIn
         return render_template("home.html")
     else:
@@ -68,42 +129,49 @@ def testLoggedIn():
     if username is None:
         abort(403)
 
+def testCanRun(command):
+    username = session.get("username")
+    if username is None:
+        abort(403)
+    if command not in accounts[username]["allowedCommands"]:
+        abort(403)
+
 @app.route('/actions/pause-on', methods=['GET'])
 def pauseStateOn():
     testLoggedIn()
-    child.sendline('pause on')
+    inputCommand('pause on')
     return redirect("/")
 
 @app.route('/actions/pause-off', methods=['GET'])
 def pauseStateOff():
     testLoggedIn()
-    child.sendline('pause off')
+    inputCommand('pause off')
     return redirect("/")
 
 @app.route('/actions/save-to-slot/<save_slot>', methods=['GET'])
 def saveToSlot(save_slot):
     testLoggedIn()
     if save_slot in ["slot0", "slot1", "slot2"]:
-        child.sendline(f'save {save_slot}')
+        inputCommand(f'save {save_slot}')
     return redirect("/")
 
 @app.route('/actions/load-slot/<save_slot>', methods=['GET'])
 def loadSlot(save_slot):
     testLoggedIn()
     if save_slot in ["slot0", "slot1", "slot2"]:
-        child.sendline(f'save {save_slot}')
+        inputCommand(f'load {save_slot}')
     return redirect("/")
 
 @app.route('/actions/stop', methods=['GET'])
 def stopGame():
     testLoggedIn()
-    child.sendline(f'stop')
+    inputCommand(f'stop')
     return redirect("/")
 
 @app.route('/actions/host', methods=['GET'])
 def hostGame():
     testLoggedIn()
-    child.sendline(f'host')
+    inputCommand(f'host')
     return redirect("/")
 
 @app.route('/actions/host', methods=['POST'])
@@ -116,38 +184,43 @@ def hostGameDefined():
         abort(400)
     if mode not in ["sandbox", "survival", "attack", "pvp"]:
         abort(400)
-    child.sendline(f'host {map} {mode}')
+    inputCommand(f'host {map} {mode}')
     return redirect("/")
 
 @app.route('/actions/runwave', methods=['GET'])
 def runwave():
     testLoggedIn()
-    child.sendline(f'runwave')
+    inputCommand(f'runwave')
     return redirect("/")
 
-@app.route('/actions/letInPlayer', methods=['GET'])
+@app.route('/actions/disableWhitelist', methods=['GET'])
 def letInPlayer():
     testLoggedIn()
-    child.sendline("config whitelist off")
+    inputCommand("config whitelist off")
     return redirect("/")
 
-@app.route('/actions/keepOut', methods=['GET'])
+@app.route('/actions/enableWhitelist', methods=['GET'])
 def keepOut():
     testLoggedIn()
-    child.sendline("config whitelist on")
+    inputCommand("config whitelist on")
     return redirect("/")
 
-@app.route('/actions/tempWhitelistOff', methods=['GET'])
+@app.route('/actions/whitelistRecentPlayer', methods=['GET'])
 def tempWhitelistOff():
     testLoggedIn()
-    child.sendline("config whitelist off")
-    time.sleep(20)
-    child.sendline("config whitelist on")
+    global conversationPointer
+    recentActivity = getOutput()
+    matches = re.findall(r"([A-Za-z=\d]{24})", recentActivity)
+    if len(matches) >= 1:
+        inputCommand(f"whitelist-add {matches[-1]}")
+    else:
+        conversation.append("No players joining recently")
     return redirect("/")
 
-@app.route('/debug', methods=['GET'])
+@app.route('/status', methods=['GET'])
 def debug():
-    return repr(child.after), 200
+    inputCommand("status")
+    return redirect("/")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
